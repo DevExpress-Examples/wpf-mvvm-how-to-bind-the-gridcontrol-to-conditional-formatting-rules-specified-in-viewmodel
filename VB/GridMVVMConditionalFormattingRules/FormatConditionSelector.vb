f4eebaa3982f92665935ff118091795d6991400d
DevExpressExampleBot <fakeEmail>
2021-01-20 16:28:31 +0300

    Source auto update [skip ci]

---
+++ VB/GridMVVMConditionalFormattingRules/FormatConditionSelector.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace GridMVVMConditionalFormattingRules
+	Public Class FormatConditionSelector
+		Inherits DataTemplateSelector
+
+		Public Property BackgroundTemplate() As DataTemplate
+		Public Property FontTemplate() As DataTemplate
+
+		Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
+			Dim rule As FormattingRule = TryCast(item, FormattingRule)
+			If rule Is Nothing Then
+				Return Nothing
+			End If
+			Select Case rule.Type
+				Case FormattingType.Font
+					Return FontTemplate
+				Case FormattingType.Background
+					Return BackgroundTemplate
+			End Select
+			Return Nothing
+		End Function
+	End Class
+End Namespace
---
+++ VB/GridMVVMConditionalFormattingRules/OrdersViewModel.vb
@@ -0,0 +1,45 @@
+﻿Imports DevExpress.Mvvm
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+
+Namespace GridMVVMConditionalFormattingRules
+	Public Enum FormattingType
+		Font
+		Background
+	End Enum
+	Public Enum ConditionRule
+		Less
+		Greater
+	End Enum
+
+	Public Class ViewModel
+		Inherits ViewModelBase
+
+		Public Sub New()
+			Orders = OrdersDataModel.GetOrders()
+			Rules = New ObservableCollection(Of FormattingRule)() From {
+				New FormattingRule("Total", ConditionRule.Less, 0, True, FormattingType.Background),
+				New FormattingRule(NameOf(Order.Discount), ConditionRule.Greater, 0, False, FormattingType.Font)
+			}
+		End Sub
+		Public ReadOnly Property Orders() As List(Of Order)
+		Public ReadOnly Property Rules() As ObservableCollection(Of FormattingRule)
+	End Class
+
+	Public Class FormattingRule
+		Inherits BindableBase
+
+		Public Sub New(ByVal fieldname As String, ByVal rule As ConditionRule, ByVal value As Integer, ByVal applytorow As Boolean, ByVal type As FormattingType)
+			Me.FieldName = fieldname
+			Me.Rule = rule
+			Me.Value = value
+			Me.ApplyToRow = applytorow
+			Me.Type = type
+		End Sub
+		Public Overridable ReadOnly Property FieldName() As String
+		Public Overridable ReadOnly Property Rule() As ConditionRule
+		Public Overridable ReadOnly Property Value() As Integer
+		Public Overridable ReadOnly Property ApplyToRow() As Boolean
+		Public Overridable ReadOnly Property Type() As FormattingType
+	End Class
+End Namespace
